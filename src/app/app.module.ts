6ccb611339d83d6adc4936a4217d437dbef021f2
vichanse <vicky.nsenga@gmail.com>
2019-09-13 05:16:23 +0200

    Using a component as a Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CareListComponent } from './cares/care-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CareListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cares/care-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'cm-cares',
+    templateUrl: './care-list.component.html'
+})
+export class CareListComponent {
+
+}
