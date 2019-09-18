c4d379b65272c8afa50c9317094882c8b48a1525
vichanse <vicky.nsenga@gmail.com>
2019-09-18 07:33:40 +0200

    Add hamburger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageTitle = 'Vichanse Health Care Management';
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
