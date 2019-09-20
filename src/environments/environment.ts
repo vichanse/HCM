432de4501cd74de72803a68f677cf4ac138f8b67
vichanse <vicky.nsenga@gmail.com>
2019-09-20 04:48:17 +0200

    Add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD4P6gwxXL_M2cDrZDXNS-2RY85ZQKW0Bk",
+    authDomain: "vichanse-hcm.firebaseapp.com",
+    databaseURL: "https://vichanse-hcm.firebaseio.com",
+    projectId: "vichanse-hcm",
+    storageBucket: "",
+    messagingSenderId: "204887609906",
+    appId: "1:204887609906:web:0dc441cb13cd5c3490222b"
+  }
 };
 
 /*
