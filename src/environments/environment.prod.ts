b67c4d2891a69f7e737830d268c2ef0261b3e236
vichanse <vicky.nsenga@gmail.com>
2019-09-21 07:57:04 +0200

    Added firebase config in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4P6gwxXL_M2cDrZDXNS-2RY85ZQKW0Bk",
+    authDomain: "vichanse-hcm.firebaseapp.com",
+    databaseURL: "https://vichanse-hcm.firebaseio.com",
+    projectId: "vichanse-hcm",
+    storageBucket: "",
+    messagingSenderId: "204887609906",
+    appId: "1:204887609906:web:0dc441cb13cd5c3490222b"
+  }
 };
